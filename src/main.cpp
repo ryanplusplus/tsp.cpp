b82996a3d0e892611970dc18d30e84982a5ff571
Ryan Hartlage <ryanplusplus@gmail.com>
2022-07-23 08:25:39 -0400

    Build multiple tours

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "map/Map.hpp"
 #include "tour/Builder.hpp"
 #include "tour/LeastCostInserter.hpp"
@@ -7,17 +8,29 @@
 using namespace std;
 using namespace tour;
 using namespace map;
+using namespace util;
 
 int main()
 {
   auto map = Map::from_file("/home/ryan/git/tsp.cpp/data/distances.csv");
 
-  auto tour = tour::Builder<tour::LeastCostInserter, util::Randomizer>::build(map);
+  Tour shortest_tour{};
+  unsigned minimum_cost = std::numeric_limits<unsigned>::max();
 
-  for(auto city : tour.cities) {
+  for(auto i = 0; i < 1000; ++i) {
+    auto tour = Builder<LeastCostInserter, Randomizer>::build(map);
+    auto cost = tour.cost(map);
+
+    if(cost < minimum_cost) {
+      shortest_tour = tour;
+      minimum_cost = cost;
+    }
+  }
+
+  for(auto city : shortest_tour.cities) {
     cout << map.city_name(city) << endl;
   }
   cout << endl;
 
-  cout << tour.cost(map) << endl;
+  cout << minimum_cost << endl;
 }
